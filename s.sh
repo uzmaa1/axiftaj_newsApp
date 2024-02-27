6722be6b499fe4f5fb9869e32b3f1b94cdc58537
uzmaa1 <68188098+uzmaa1@users.noreply.github.com>
2024-02-28 04:51:24 +0500

    Update s.sh

--- s.sh
+++ s.sh
@@ -1,3 +1,13 @@
 chmod +x s.sh
 shopt -s extglob
-rm -v !("flutter_travel_ui_tutorial.zip"|".devcontainer"|"s.sh")
+rm -r -v !("flutter_travel_ui_tutorial.zip"|".devcontainer"|"s.sh")
+unzip flutter_travel_ui_tutorial.zip
+$D="flutter_travel_ui_tutorial"
+cd $D
+echo "files in $D"
+ls
+mv ./* ../
+echo "afer mv files in $D"
+ls
+#rm -r -v "__MACOSX/flutter_travel_ui_tutorial"
+#shopt -u extglob
