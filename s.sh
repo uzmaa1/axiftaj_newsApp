b134d50719b9fc3fae0dc71f72b6e718c3776d99
uzmaa1 <68188098+uzmaa1@users.noreply.github.com>
2024-02-28 00:29:40 +0000

    files set

--- s.sh
+++ s.sh
@@ -1,11 +1,5 @@
-chmod +x s.sh
-shopt -s extglob
-rm -r -v !("flutter_travel_ui_tutorial.zip"|".devcontainer"|"s.sh")
-unzip flutter_travel_ui_tutorial.zip
-$D="flutter_travel_ui_tutorial"
-cd $D
-echo "files in $D"
-ls
+#chmod +x s.sh
+6ls
 mv ./* ../
 echo "afer mv files in $D"
 ls
